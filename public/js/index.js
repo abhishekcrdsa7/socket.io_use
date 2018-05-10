2df836148b755a54508a363c0c3675623c69a1bb
abhishekcrdsa7 <abhishekcrdsa7@gmail.com>
2018-05-11 01:10:30 +0530

    Add message utils

--- public/js/index.js
+++ public/js/index.js
@@ -3,4 +3,8 @@ socket.on('connect',() => {
   console.log("connected to the server.");
 socket.on('newMessage',(message) => {console.log(message)});
 });
+
+socket.emit('createMessage',{from: "ashu", text: "this is ashu"},(data) => {
+  console.log('got it'+data);
+});
 socket.on('disconnect',() => {console.log("disconnectd from the server.")});
--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const socketIO = require('socket.io');
 const http = require('http');
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
+const {generateMessage} = require('./utils/message');
 var app = express();
 var server = http.createServer(app);
 var io = socketIO(server);
@@ -11,12 +12,13 @@ app.use(express.static(publicPath));
 
 io.on('connection',(socket) => {console.log('New Connection');
 
-socket.emit('newMessage',{from: "Admin",text: "Welcome to the chat room."});
-socket.broadcast.emit('newMessage',{from: "Admin",text: "new user connected to the chat room."});
+socket.emit('newMessage',generateMessage('Admin','Welcome to the chat room.'));
+socket.broadcast.emit('newMessage',generateMessage('Admin','new user connected to the chat room.'));
 
-socket.on('createMessage',(message) => {
-  console.log(message);
-  io.emit('newMessage',{...message,createdAt: new Date()});
+socket.on('createMessage',(message,callback) => {
+  let {from,text} = message;
+  io.emit('newMessage',generateMessage(from,text));
+  callback('this is from the server');
 })
 socket.on('disconnect',() => {console.log("client disconnectd")});
 
